efb3a6e91a81215abaec17e9227c11fd4df4a7df
Valentina Méndez <valentina.mdz@gmail.com>
2020-09-16 21:09:44 +0200

    Implement changes in homework JS3 W1

--- javascript/javascript3/week1/script.js
+++ javascript/javascript3/week1/script.js
@@ -7,85 +7,42 @@ const button = document.querySelector("button");
 const input = document.querySelector("input");
 const weatherInfo = document.querySelector(".weather-info");
 
-const dummyData = {
-  //Remove this
-  coord: { lon: 12.57, lat: 55.68 },
-  weather: [{ id: 800, main: "Clear", description: "clear sky", icon: "01n" }],
-  base: "stations",
-  main: {
-    temp: 289.82,
-    feels_like: 289.85,
-    temp_min: 288.15,
-    temp_max: 290.93,
-    pressure: 1025,
-    humidity: 88,
-  },
-  visibility: 10000,
-  wind: { speed: 2.1, deg: 200 },
-  clouds: { all: 0 },
-  dt: 1600114080,
-  sys: {
-    type: 1,
-    id: 1575,
-    country: "DK",
-    sunrise: 1600058418,
-    sunset: 1600104620,
-  },
-  timezone: 7200,
-  id: 2618425,
-  name: "Copenhagen",
-  cod: 200,
-};
-
-const dummyPromise = Promise.resolve(dummyData); //Remove this
-
-const displayWeather = () => {
-  const city = input.value;
-  fetch(getUrl(city)) //This is the good stuff
-    .then((response) => response.json()) //This is the good stuff
+const displayWeather = (evt) => {
+  evt.preventDefault();
+  if (input.value.trim() === "") {
+    weatherInfo.hidden = true;
+    return;
+  }
+  fetch(getUrl(input.value))
+    .then((response) => response.json())
     .then((data) => {
-      const icons = data.weather.map((values) => values.icon);
-      const getIconsValues = () => {
-        for (let i = 0; i < icons.length; i++) {
-          const icon = document.querySelector(".icons"); //Doubt about displaying multiple icons and avoid creating more images when clicking the button
-          icon.src = `http://openweathermap.org/img/wn/${icons[i]}@2x.png`;
-          icon.style.width = 20;
-          weatherInfo.appendChild(icon);
-        }
-      };
-      getIconsValues();
+      weatherInfo.hidden = false;
+      const $icons = document.querySelector(".icons");
+      $icons.innerHTML = "";
+      for (const weather of data.weather) {
+        const $icon = document.createElement("img");
+        $icon.src = `http://openweathermap.org/img/wn/${weather.icon}@2x.png`;
+        $icon.style.width = 20;
+        $icons.appendChild($icon);
+      }
 
-      const cityName = document.querySelector(".city-name");
-      cityName.innerHTML = `City: ${data.name}`;
-      weatherInfo.appendChild(cityName);
-
-      const temperature = document.querySelector(".temperature");
-      temperature.innerHTML = `Temperature: ${data.main.temp} °C`;
-      weatherInfo.appendChild(temperature);
-
-      const windSpeed = document.querySelector(".wind-speed");
-      windSpeed.innerHTML = `Wind speed: ${data.wind.speed}`;
-      weatherInfo.appendChild(windSpeed);
-
-      const cloudiness = document.querySelector(".cloudiness");
-      cloudiness.innerHTML = `Cloudiness: ${data.clouds.all}%`;
-      weatherInfo.appendChild(cloudiness);
-
-      const convertDay = (seconds) => {
-        const date = new Date(seconds * 1000);
-        return date.toLocaleTimeString();
+      const displayProp = (selector, txt) => {
+        const property = document.querySelector(selector);
+        property.innerHTML = txt;
       };
 
-      const sunrise = document.querySelector(".sunrise");
-      sunrise.innerHTML = `Sunrise: ${convertDay(data.sys.sunrise)}`;
-      weatherInfo.appendChild(sunrise);
+      const secondsInLocale = (seconds) =>
+        new Date(seconds * 1000).toLocaleTimeString();
 
-      const sunset = document.querySelector(".sunset");
-      sunset.innerHTML = `Sunset: ${convertDay(data.sys.sunset)}`;
-      weatherInfo.appendChild(sunset);
-    }); //Remove dummyPromise
+      [
+        [".city-name", data.name],
+        [".temperature", data.main.temp],
+        [".wind-speed", data.wind.speed],
+        [".cloudiness", data.clouds.all],
+        [".sunrise", secondsInLocale(data.sys.sunrise)],
+        [".sunset", secondsInLocale(data.sys.sunset)],
+      ].forEach(([selector, txt]) => displayProp(selector, txt));
+    });
 };
 
-button.addEventListener("click", displayWeather);
-
-// Copenhagen
+document.querySelector("form").addEventListener("submit", displayWeather);
