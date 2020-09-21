5239345f15cca73a50a90c94441b3afc87d68d81
Valentina Méndez <valentina.mdz@gmail.com>
2020-09-21 23:58:03 +0200

    Implement chnages in homework JS3 W1

--- javascript/javascript3/week1/script.js
+++ javascript/javascript3/week1/script.js
@@ -6,16 +6,30 @@ const getUrl = (city) =>
 const button = document.querySelector("button");
 const input = document.querySelector("input");
 const weatherInfo = document.querySelector(".weather-info");
+const footer = document.querySelector("footer");
+const alerts = document.querySelector(".alerts");
+const alertText = document.createElement("p");
 
 const displayWeather = (evt) => {
   evt.preventDefault();
   if (input.value.trim() === "") {
+    footer.hidden = true;
     weatherInfo.hidden = true;
+    alertText.innerHTML = "Please enter a city";
+    alerts.appendChild(alertText);
     return;
   }
   fetch(getUrl(input.value))
     .then((response) => response.json())
     .then((data) => {
+      if (data.cod === "404") {
+        weatherInfo.hidden = true;
+        footer.hidden = true;
+        alertText.innerHTML = data.message;
+        alerts.appendChild(alertText);
+        return;
+      }
+
       weatherInfo.hidden = false;
       const $icons = document.querySelector(".icons");
       $icons.innerHTML = "";
@@ -27,24 +41,25 @@ const displayWeather = (evt) => {
       }
 
       const displayProp = ([selector, txt]) => {
+        alertText.innerHTML = "";
         const property = document.querySelector(selector);
         property.innerHTML = txt;
+        footer.hidden = false;
       };
 
       const secondsInLocale = (seconds) =>
         new Date(seconds * 1000).toLocaleTimeString();
 
       [
-        [".city-name", data.name],
-        [".country", data.sys.country],
-        [".temperature", data.main.temp],
-        [".wind-speed", data.wind.speed],
-        [".cloudiness", data.clouds.all],
-        [".sunrise", secondsInLocale(data.sys.sunrise)],
-        [".sunset", secondsInLocale(data.sys.sunset)],
+        [".city-name", `City: ${data.name}`],
+        [".country", `Country: ${data.sys.country}`],
+        [".temperature", `Temperature: ${data.main.temp} °C`],
+        [".wind-speed", `Wind speed: ${data.wind.speed} m/s`],
+        [".cloudiness", `Cloudiness: ${data.clouds.all} %`],
+        [".sunrise", `Sunrise: ${secondsInLocale(data.sys.sunrise)} *`],
+        [".sunset", `Sunset: ${secondsInLocale(data.sys.sunset)} *`],
       ].forEach(displayProp);
     });
-    document.querySelector('footer').hidden = false;
 };
 
 document.querySelector("form").addEventListener("submit", displayWeather);
