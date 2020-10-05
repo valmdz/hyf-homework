541b83a94c0bfe8f25995344a41396ebdc4c2c3e
Valentina Méndez <valentina.mdz@gmail.com>
2020-10-05 15:14:24 +0200

    Implement homework Databases Week1

---
+++ databases/week1/week1.sql
@@ -0,0 +1,53 @@
+ SELECT * FROM test.task;
+
+-- Find out how many tasks are in the task table
+ SELECT COUNT(id) FROM test.task;
+
+-- Find out how many tasks in the task table do not have a valid due date
+ SELECT COUNT(id) FROM test.task 
+ WHERE due_date 
+ IS NULL;
+
+-- Find all the tasks that are marked as done
+ SELECT * FROM test.task 
+ WHERE status_id = 3;
+
+-- Find all the tasks that are not marked as done
+ SELECT * FROM test.task 
+ WHERE status_id != 3;
+
+-- Get all the tasks, sorted with the most recently created first
+ SELECT * FROM test.task 
+ ORDER BY created;
+
+-- Get the single most recently created task
+SELECT * FROM test.task 
+ORDER BY created DESC 
+LIMIT 1;
+
+-- Get the title and due date of all tasks where the title or description contains database
+SELECT title, due_date, `description`
+FROM test.task
+WHERE `description` LIKE '%database%' 
+OR title LIKE '%database%';
+
+-- Get the title and status (as text) of all tasks
+SELECT title, name
+FROM test.task
+JOIN test.status
+ON test.task.status_id = test.status.id;
+
+-- Get the name of each status, along with a count of how many tasks have that status
+SELECT COUNT(*), name
+FROM task
+JOIN status
+ON task.status_id = status.id
+GROUP BY name;
+
+-- Get the names of all statuses, sorted by the status with most tasks first
+SELECT COUNT(*) AS quantity , name
+FROM task
+JOIN status
+ON task.status_id = status.id
+GROUP BY name
+ORDER BY quantity DESC;
