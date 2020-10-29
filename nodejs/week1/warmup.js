d537939894b10dbcacf586f3676e6bb926fd0a3f
Valentina Méndez <valentina.mdz@gmail.com>
2020-10-29 18:48:21 +0100

    Solve homework nodejs week1

---
+++ nodejs/week1/warmup.js
@@ -0,0 +1,19 @@
+class Circle {
+  constructor(radius) {
+    this.radius = radius;
+  }
+  getDiameter() {
+    return this.radius * 2;
+  }
+  getCircumference() {
+    return 2 * Math.PI * this.radius;
+  }
+  getArea() {
+    return Math.PI * Math.pow(this.radius, 2);
+  }
+}
+
+const circle = new Circle(10);
+console.log(circle.getDiameter()); // 20
+console.log(circle.getCircumference()); 
+console.log(circle.getArea()); 
