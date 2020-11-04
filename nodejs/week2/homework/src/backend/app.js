0d2635389cebb62aec3b4c8743024a1941d8f256
Valentina Méndez <valentina.mdz@gmail.com>
2020-11-04 21:39:14 +0100

    Handle meals, all tests pass nodejs week2

--- nodejs/week2/homework/src/backend/app.js
+++ nodejs/week2/homework/src/backend/app.js
@@ -6,22 +6,19 @@ const meals = require("./data/meals");
 const reviews = require("./data/reviews");
 const reservations = require("./data/reservations");
 
+const mealsWithReviews = require("./mealsWithReviews");
+
 // Meals router
 const MealsRouter = require("./meals-router");
 app.use("/meals", MealsRouter.router);
 
 // Reviews router
-//const reviewsRouter = require("./reviews-router");
-//app.use("/reviews", reviewsRouter);
+const reviewsRouter = require("./reviews-router");
+app.use("/reviews", reviewsRouter.router);
 
 // reservation router
-// const reservationsRouter = require("./reservations-router");
-// app.use("/reservations", reservationsRouter);
-
-const mealsWithReviews = meals.map((meal) => {
-  meal.reviews = reviews.filter((review) => review.mealId === meal.id);
-  return meal;
-});
+const reservationsRouter = require("./reservations-router");
+app.use("/reservations", reservationsRouter.router);
 
 /** `pickRandom(xs)` returns a random element of `xs` where `xs` is an array. */
 const pickRandom = (xs) => xs[Math.floor(Math.random() * xs.length)];
@@ -30,10 +27,6 @@ app.get("/", async (_request, response, next) => {
   response.send("Meal Sharing Web App");
   next();
 });
-
-// app.get("/meals", async (_request, response) => {
-//   response.send(mealsWithReviews);
-// });
 
 app.get("/cheap-meals", async (_request, response) => {
   response.send(mealsWithReviews.filter((meal) => meal.price < 60));
@@ -45,14 +38,6 @@ app.get("/large-meals", async (_request, response) => {
 
 app.get("/meal", async (_request, response) => {
   response.send(pickRandom(mealsWithReviews));
-});
-
-app.get("/reviews", async (_request, response) => {
-  response.send(mealsWithReviews);
-});
-
-app.get("/reservations", async (_request, response) => {
-  response.send(reservations);
 });
 
 app.get("/reservation", async (_request, response) => {
--- nodejs/week2/homework/src/backend/meals-router.js
+++ nodejs/week2/homework/src/backend/meals-router.js
@@ -1,13 +1,79 @@
+const { request } = require("express");
 const express = require("express");
 const router = express.Router();
 
 const mealsWithReviews = require("./mealsWithReviews");
 
-router.get("/", async (_request, response) => {
-  response.send(mealsWithReviews);
+const mealsUnderMaxPrice = (request, meals) => {
+  if (request.query.maxPrice === undefined) {
+    return meals;
+  }
+  const maxPrice = parseInt(request.query.maxPrice);
+  if (isNaN(maxPrice)) {
+    return;
+  }
+  return meals.filter((meal) => meal.price < maxPrice);
+};
+
+const matchingTitles = (request, meals) => {
+  if (request.query.title === undefined) {
+    return meals;
+  }
+  return meals.filter((meal) => meal.title.includes(request.query.title));
+};
+
+const createdAfter = (request, meals) => {
+  if (request.query.createdAfter === undefined) {
+    return meals;
+  }
+  const date = new Date(request.query.createdAfter);
+  if (isNaN(Number(date))) {
+    return;
+  }
+  return meals.filter((meal) => date <= new Date(meal.createdAt));
+};
+
+const limitArray = (request, meals) => {
+  if (request.query.limit === undefined) {
+    return meals;
+  }
+  const limit = parseInt(request.query.limit);
+  if (isNaN(limit)) {
+    return;
+  }
+  return meals.slice(0, limit);
+};
+
+// Application of filtering with queries
+router.get("/", async (request, response) => {
+  const meals0 = mealsUnderMaxPrice(request, mealsWithReviews);
+  if (meals0 === undefined) {
+    response.status(400);
+    response.send();
+    return;
+  }
+  const meals1 = matchingTitles(request, meals0);
+  if (meals1 === undefined) {
+    response.status(400);
+    response.send();
+    return;
+  }
+  const meals2 = createdAfter(request, meals1);
+  if (meals2 === undefined) {
+    response.status(400);
+    response.send();
+    return;
+  }
+  const meals3 = limitArray(request, meals2);
+  if (meals3 === undefined) {
+    response.status(400);
+    response.send();
+    return;
+  }
+  response.send(meals3);
 });
 
-// Filter meal according to ID
+// Find meal according to ID
 router.get("/:id", async (request, response) => {
   const id1 = parseInt(request.params.id);
   if (!id1) {
@@ -17,8 +83,9 @@ router.get("/:id", async (request, response) => {
   }
   const aMeal = mealsWithReviews.find((meal) => meal.id === id1);
   if (aMeal === undefined) {
-    response.status(400);
-    response.send({ message: `There are no meals with the id:${id1}` });
+    // response.status(400);
+    // response.send({ message: `There are no meals with the id: ${id1}` });
+    response.send();
     return;
   }
   response.send(aMeal);
